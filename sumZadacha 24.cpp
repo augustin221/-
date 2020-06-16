4a8915d1e915b5c39145598322d20bb7519c7467
augustin <54428095+augustin221@users.noreply.github.com>
2020-06-16 19:00:38 +0300

    Add files via upload

---
+++ sumZadacha 24.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int mian(int argc , char* argv[] ) {
+	double suma = 0
+		int n;
+	cout << "введитье хуй";
+	cin >> n;
+
+	for (int countrer = 1; counter <= n; counter++) {
+		sum += 1 / suma(counter);
+	}
+	cout << "summa blyat" << sum << endl;
+	system("pause");
+	return 0;
+}
+
+double sumSin(int n)  {
+	double p = M_PI;
+	double temp_sum = 0;
+
+	for (int counter = 1; counter*p / 180);
+	temp_sum += sun((double)counter*p / 180);
+	return temp_sum;
+}
