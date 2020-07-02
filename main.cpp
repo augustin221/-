7d63cb82baba9bd6e129e2e7b8ce81beb3e6c164
augustin <54428095+augustin221@users.noreply.github.com>
2020-07-02 11:24:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include  <iostream>
+using namespace std;
+
+int main() {
+    int grade = 0;
+    int aCount = 0, bCount, cCount;
+    cout << "введите букуву какуетеу для оценки ёпты бля" << endl;
+    cout << "гыы метал музыка настоящих мужчин"<<  endl;
+    
+    while() {
+        switch ((grade = cin(get() )) != EOF ) {
+            switch grade {
+                case 'A': case 'a':
+                    ++aCount;
+                    break;
+                case 'B': case 'b':
+                    ++bCount;
+                    break;
+                case 'C': case 'c':
+                    ++cCount;
+                    break;
+            }
+        }
+    }
+}
